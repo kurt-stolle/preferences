51624435b48ee85a7f18309949323be7ccffd66b
Kurt Stolle <k.h.w.stolle@tue.nl>
2024-02-12 15:55:18 +0000

    Updated packages

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -130,7 +130,7 @@ require("lazy").setup({
     "lervag/vimtex", 
     init = function()
       vim.g.vimtex_options = "go here"
-      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_view_method = "zathura_simple"
     end
   },
 
