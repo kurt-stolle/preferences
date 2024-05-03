be4c173eafd2d4bb3d3153008d01406d97f673a6
Kurt Stolle <kurt-stolle@users.noreply.github.com>
2024-05-04 00:37:00 +0200

    update aliases

--- config/shenv.d/10-alias.sh
+++ config/shenv.d/10-alias.sh
@@ -7,6 +7,9 @@ alias dnf='sudo dnf'
 alias apt='sudo apt'
 alias apt-get='sudo apt-get'
 
+# Git
+alias gf='git submodule foreach'
+
 # LS
 alias la='ls -Alh' # show hidden files
 alias ls='ls -aFh --color=always' # add colors and file type extensions
