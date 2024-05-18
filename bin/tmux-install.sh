4b894d367c1f5d737458ac5d48a6e9816ba0cf88
Kurt Stolle <kurt-stolle@users.noreply.github.com>
2024-05-18 14:30:49 +0200

    Updated Tmux installation script for user environments

--- bin/tmux-install.sh
+++ bin/tmux-install.sh
@@ -10,6 +10,7 @@ PREFIX="$HOME/.local"
 
 mkdir -p "$PREFIX"
 
+echo "Installing tmux version ${TMUX_VERSION} to ${PREFIX}..."
 TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t '.tmp')
 if [[ ! "$TMP_DIR" || ! -d "$TMP_DIR" ]]; then
     echo "Could not create temporary working directory!"
@@ -21,7 +22,36 @@ function cleanup {
 }
 trap cleanup EXIT
 
+# Go to working directory
 cd "$TMP_DIR"
+
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+
+# Check whether `ncurses` is installed, else install it from source
+if ! command -v ncurses-config &> /dev/null; then
+    echo "Installing ncurses from source..."
+    wget "https://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz" -O "ncurses.tar.gz"
+    tar -xzf ncurses.tar.gz
+    cd ncurses-*
+    ./configure --prefix="$PREFIX" --with-shared --enable-widec --with-termlib --enable-pc-files --with-pkg-config-libdir="$PKG_CONFIG_PATH"
+    make -j"$(nproc)" && make install
+    cd "$TMP_DIR"
+fi
+
+
+# Check whether `libevent` is installed, else install it from source
+if ! command -v libevent-config &> /dev/null; then
+    echo "Installing libevent from source..."
+    wget "https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz" -O "libevent.tar.gz"
+    tar -xzf "libevent.tar.gz"
+    cd libevent-*
+    ./configure --prefix="$PREFIX" --enable-shared
+    make -j"$(nproc)" && make install
+    cd "$TMP_DIR"
+fi
+
+# Install `tmux` from source
+echo "Installing tmux from source..."
 wget "https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz" -O "tmux.tar.gz"
 tar -xzf tmux.tar.gz
 cd "tmux-${TMUX_VERSION}"
