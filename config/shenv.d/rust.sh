dd25d0785acfbaddc0e6a251fc5b81613afc90b8
Kurt Stolle <k.h.w.stolle@gmail.com>
2024-06-07 11:37:12 +0200

    Updated windows/WSL configurations

--- config/shenv.d/rust.sh
+++ config/shenv.d/rust.sh
@@ -1,2 +1,14 @@
-export PATH=$PATH:$HOME/.cargo/bin
+# Check if cargo is installed
+if ! [ -d "$HOME/.cargo" ]; then
+    return
+fi
 
+# Check whether cargo is in the PATH
+if ! [[ "$PATH" =~ "$HOME/.cargo/bin" ]]
+then
+    PATH="$HOME/.cargo/bin:$PATH"
+fi
+export PATH
+
+# Source cargo environment
+source "$HOME/.cargo/env"
